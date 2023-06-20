91bf8ac8b55cccf7ff3a021f343f9dc7c7b5ceb6
GeralBot <133059729+GeralBot@users.noreply.github.com>
2023-06-20 17:49:26 -0500

    GeralBot

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,7 +5,7 @@ throw false
 }
 let pesan = args.join` `
 let oi = `@𝐠𝐞𝐫𝐚𝐥_𝐯𝐞𝐧𝐭𝐚𝐬𝟏𝟖 : ${pesan}`
-let teks = `Creador Wa.me/51935763050 SI QUIERES BOT COMUNICAME
+let teks = `Creador Wa.me/51935763050 SI QUIERES BOT COMUNICAME 
 𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐁𝐨𝐭𝐬 - 𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭 \n\n❏ ${oi}\n\n❏ 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬: \n`
 for (let mem of participants) {
 teks += `🩵 @${mem.id.split('@')[0]}\n`}
